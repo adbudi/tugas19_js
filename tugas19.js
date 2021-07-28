13fd3adf401797480ca44bb646b519886000549d
as <adbudi1@gmail.com>
2021-07-28 10:54:52 +0700

    tugas 19 javascript
    tugas ini di kumpulkan untuk niomic

---
+++ tugas19.js
@@ -0,0 +1,14 @@
+function tugas19() {
+  console.log('Semangat.....'.repeat(5));
+  console.log('*'.repeat(5));
+  console.log('*'.repeat(25));
+  console.log('*'.repeat(45));
+  console.log('*'.repeat(65));
+  console.log('*'.repeat(95));
+  console.log('*'.repeat(65));
+  console.log('*'.repeat(45));
+  console.log('*'.repeat(25));
+  console.log('*'.repeat(5));
+}
+
+tugas19()
